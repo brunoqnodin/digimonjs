6c82fc8bf6bb1e723cf03de26c768e30b45c916e
brunoqnodin <brunoqnodin@gmail.com>
2023-02-01 15:27:03 -0300

    Atualizações

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,15 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import TableDigimon from "./Home/TableDigimon";
+import Home from "./Home/Home";
 import AppBarDigimon from "./components/AppBarDigimon";
-import CardDigimon from "./components/CardDigimon";
-import { Grid } from "@mui/material";
-import { Container } from "@mui/system";
 
 function App() {
-  const [digimons, setDigimons] = useState([]);
-
-  const getDigimons = () => {
-    axios
-      .get("https://digimon-api.vercel.app/api/digimon")
-      .then((response) => setDigimons(response.data))
-      .catch((response) => console.log(response));
-  };
-
-  useEffect(() => {
-    getDigimons()
-  }, [])
-
   return (
     <div>
-      <AppBarDigimon />
-      <Container maxWidth="false">
-        <Grid container spacing={2}>
-          {digimons.map((item, i) => (
-            <Grid item xs={2}>
-              <CardDigimon name={item.name} level={item.level} image={item.img}/>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
+    <AppBarDigimon/>
+    <Home/>
     </div>
-  );
+  )
 }
 
 export default App;
